b883d8ad06e7f3cd5f79b14dbde94e3646708b19
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-24 20:57:17 +0200

    feat: add more dummy data to show how the product grid shows more rows

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,167 @@ const App = (): JSX.Element => {
         currency: 'EUR',
         image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
       }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
+    ],
+    [
+      {
+        id: 12,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/1538/310/400/2/w/248/1538310400_15_1_1.jpg?ts=1690535015005'
+      },
+      {
+        id: 13,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      },
+      {
+        id: 14,
+        name: 'JEANS SLIM CROPPED',
+        price: 39.95,
+        currency: 'EUR',
+        image: 'https://static.zara.net/photos///2023/I/0/2/p/8062/333/427/2/w/468/8062333427_15_1_1.jpg?ts=1692355484140'
+      }
     ]
   ];
 
