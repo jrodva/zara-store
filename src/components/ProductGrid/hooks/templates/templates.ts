6d004e8b0621ea92e539fed7d17d6f87c54981f6
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-29 18:57:28 +0200

    feat: add new hook to manage the templates

---
+++ src/components/ProductGrid/hooks/templates/templates.ts
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react'
+import { TemplatesHook } from './types.ts'
+import { getTemplates } from '../../../../services/templatesService.ts'
+import { Template } from '../../../../interfaces/Template.ts'
+
+export const useTemplates = (): TemplatesHook => {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isError, setIsError] = useState<boolean>(false)
+  const [templates, setTemplates] = useState<Template[]>([])
+
+  useEffect((): void => {
+    getTemplates()
+      .then(response => response.json())
+      .then((templates: Template[]): void => {
+        setTemplates(templates)
+        setIsLoading(false)
+      })
+      .catch((): void => {
+        setIsLoading(false)
+        setIsError(true)
+      })
+  }, [])
+
+  return {
+    templates,
+    isError,
+    isLoading
+  }
+}
